fa61eadc0eae289f8d2ff4936fc4f2623e6af578
zens <wang.pei.zens@gmail.com>
2024-08-12 23:48:58 +0800

    [add] create react project

---
+++ src/app/pages/DemoPage.js
@@ -0,0 +1,7 @@
+export const DemoPage = () => {
+    return (
+        <div>
+            DemoPage
+        </div>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { DemoPage } from './app/pages/DemoPage';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <DemoPage />
+);
